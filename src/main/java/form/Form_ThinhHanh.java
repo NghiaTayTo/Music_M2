96beac1a4f17f186788a8f811e963f003604c41b
TRONG NGHIA <TRONG NGHIA@192.168.1.23>
2023-11-07 22:19:30 +0700

    Init Project

--- src/main/java/form/Form_ThinhHanh.java
+++ src/main/java/form/Form_ThinhHanh.java
@@ -26,12 +26,22 @@ public class Form_ThinhHanh extends javax.swing.JLayeredPane {
 
         slideShow1 = new SlideShow.SlideShow();
         vuTruNhacViet2 = new List.VuTruNhacViet();
+        quocTeNoiBat2 = new List.QuocTeNoiBat();
+        tamTrangHomNay1 = new List.TamTrangHomNay();
+        chuDeHot2 = new List.ChuDeHot();
+        top1001 = new List.Top100();
+        videoHot1 = new List.VideoHot();
 
         setBackground(new java.awt.Color(29, 38, 49));
         setOpaque(true);
 
         setLayer(slideShow1, javax.swing.JLayeredPane.DEFAULT_LAYER);
         setLayer(vuTruNhacViet2, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        setLayer(quocTeNoiBat2, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        setLayer(tamTrangHomNay1, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        setLayer(chuDeHot2, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        setLayer(top1001, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        setLayer(videoHot1, javax.swing.JLayeredPane.DEFAULT_LAYER);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -43,7 +53,13 @@ public class Form_ThinhHanh extends javax.swing.JLayeredPane {
                 .addGap(41, 41, 41))
             .addGroup(layout.createSequentialGroup()
                 .addGap(20, 20, 20)
-                .addComponent(vuTruNhacViet2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(videoHot1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(top1001, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(chuDeHot2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(tamTrangHomNay1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(quocTeNoiBat2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(vuTruNhacViet2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -52,13 +68,28 @@ public class Form_ThinhHanh extends javax.swing.JLayeredPane {
                 .addComponent(slideShow1, javax.swing.GroupLayout.PREFERRED_SIZE, 289, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(26, 26, 26)
                 .addComponent(vuTruNhacViet2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(1082, Short.MAX_VALUE))
+                .addGap(40, 40, 40)
+                .addComponent(quocTeNoiBat2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(40, 40, 40)
+                .addComponent(tamTrangHomNay1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(40, 40, 40)
+                .addComponent(chuDeHot2, javax.swing.GroupLayout.PREFERRED_SIZE, 262, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(40, 40, 40)
+                .addComponent(top1001, javax.swing.GroupLayout.PREFERRED_SIZE, 270, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(videoHot1, javax.swing.GroupLayout.PREFERRED_SIZE, 765, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(442, 442, 442))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private List.ChuDeHot chuDeHot2;
+    private List.QuocTeNoiBat quocTeNoiBat2;
     private SlideShow.SlideShow slideShow1;
+    private List.TamTrangHomNay tamTrangHomNay1;
+    private List.Top100 top1001;
+    private List.VideoHot videoHot1;
     private List.VuTruNhacViet vuTruNhacViet2;
     // End of variables declaration//GEN-END:variables
 }
